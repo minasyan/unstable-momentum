1b4f83aa1ee29b4d20af3dcf2c06d0535d87d6c5
Tarek7 <tmansour@mit.edu>
2018-05-18 00:22:05 +0000

    Implemented an architecture specific to CIFAR

--- CIFAR.py
+++ CIFAR.py
@@ -11,40 +11,37 @@ from matplotlib import pyplot as plt
 class Net1(nn.Module):
     def __init__(self):
         super(Net1, self).__init__()
-        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
-        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.conv1 = nn.Conv2d(3, 10, kernel_size=5)
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5, padding=2) 
+        self.conv3 = nn.Conv2d(20, 20, kernel_size=5)
         self.conv2_drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(320, 50)
+        self.fc1 = nn.Linear(500, 50)
         self.fc2 = nn.Linear(50, 10)
 
     def forward(self, x):
         x = F.relu(F.max_pool2d(self.conv1(x), 2))
-        print(x.shape)
-        x = F.relu(F.max_pool2d(self.conv2(x), 2))
-        print(x.shape)
-        x = x.view(-1, 320)
-        print(x.shape)
+        x = F.relu(self.conv2(x))
+        x = F.relu(F.max_pool2d(self.conv3(x), 2))
+        x = x.view(-1, 500)
         x = F.relu(self.fc1(x))
-        print(x.shape)
         x = self.fc2(x)
-        print(x.shape)
-
-        FUCK EDGAR
         return F.log_softmax(x, dim=1)
 
 class Net2(nn.Module):
     def __init__(self):
         super(Net2, self).__init__()
-        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
-        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.conv1 = nn.Conv2d(3, 10, kernel_size=5)
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5, padding=2) 
+        self.conv3 = nn.Conv2d(20, 20, kernel_size=5)
         self.conv2_drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(320, 50)
+        self.fc1 = nn.Linear(500, 50)
         self.fc2 = nn.Linear(50, 10)
 
     def forward(self, x):
         x = F.relu(F.max_pool2d(self.conv1(x), 2))
-        x = F.relu(F.max_pool2d(self.conv2(x), 2))
-        x = x.view(-1, 320)
+        x = F.relu(self.conv2(x))
+        x = F.relu(F.max_pool2d(self.conv3(x), 2))
+        x = x.view(-1, 500)
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
         return F.log_softmax(x, dim=1)
